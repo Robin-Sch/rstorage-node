477a183bb769140f1bce05eb5f2c26a76e814a1e
RobinSchapendonk <46354497+Robin-floss@users.noreply.github.com>
2021-08-17 15:33:45 +0200

    https

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const { join } = require('path');
 const { readFileSync, writeFileSync, existsSync, unlinkSync, mkdirSync, createReadStream } = require('fs');
 const multer = require('multer');
+const { createCertificate } = require('pem');
+const { createServer } = require('https');
 
 const storage = multer.diskStorage({
 	destination: (req, file, cb) => {
@@ -15,30 +17,39 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
-const { generateKeys, unpack, pack } = require('./crypt.js');
-
-const { NOT_ENCRYPTED_REQUEST, ALREADY_CONNECTED_TO_PANEL, NOT_CONNECTED_TO_PANEL, NO_SUCH_FILE_OR_DIR, INVALID_BODY, SUCCESS } = require('../responses.json');
+const { ALREADY_CONNECTED_TO_PANEL, NOT_CONNECTED_TO_PANEL, NO_SUCH_FILE_OR_DIR, INVALID_BODY, SUCCESS } = require('../responses.json');
 
 const {
 	NODE_PORT,
 } = process.env;
 
 const port = NODE_PORT || 3001;
+const dir = join(__dirname, '../', 'files');
 
 if (!existsSync(join(__dirname, '../', 'keys'))) mkdirSync(join(__dirname, '../', 'keys'));
 if (!existsSync(join(__dirname, '../', 'files'))) mkdirSync(join(__dirname, '../', 'files'));
-let NODE_PRIVATE_KEY = existsSync(join(__dirname, '../', 'keys/rsa_key')) ? readFileSync(join(__dirname, '../', 'keys/rsa_key'), 'utf8') : null;
-let NODE_PUBLIC_KEY = existsSync(join(__dirname, '../', 'keys/rsa_key.pub')) ? readFileSync(join(__dirname, '../', 'keys/rsa_key.pub'), 'utf8') : null;
-let SERVER_PUBLIC_KEY = existsSync(join(__dirname, '../', 'keys/server_rsa_key.pub')) ? readFileSync(join(__dirname, '../', 'keys/server_rsa_key.pub'), 'utf8') : null;
+let NODE_KEY = existsSync(join(__dirname, '../', 'keys/node.key')) ? readFileSync(join(__dirname, '../', 'keys/node.key'), 'utf8') : null;
+let NODE_CA = existsSync(join(__dirname, '../', 'keys/ca.key')) ? readFileSync(join(__dirname, '../', 'keys/ca.key'), 'utf8') : null;
+let NODE_CERT = existsSync(join(__dirname, '../', 'keys/node.cert')) ? readFileSync(join(__dirname, '../', 'keys/node.cert'), 'utf8') : null;
+let PANEL_KEY = existsSync(join(__dirname, '../', 'keys/panel')) ? readFileSync(join(__dirname, '../', 'keys/panel'), 'utf8') : null;
 
-if (!NODE_PRIVATE_KEY || !NODE_PUBLIC_KEY) {
-	const keys = generateKeys();
+if (!NODE_KEY || !NODE_CA || !NODE_CERT) {
+	return createCertificate({ selfSigned: true }, (err, keys) => {
+		if (err) throw err;
 
-	NODE_PRIVATE_KEY = keys.private;
-	NODE_PUBLIC_KEY = keys.public;
+		NODE_KEY = keys.serviceKey;
+		NODE_CA = keys.clientKey;
+		NODE_CERT = keys.certificate;
 
-	writeFileSync(join(__dirname, '../', 'keys/rsa_key'), NODE_PRIVATE_KEY);
-	writeFileSync(join(__dirname, '../', 'keys/rsa_key.pub'), NODE_PUBLIC_KEY);
+		writeFileSync(join(__dirname, '../', 'keys/node.key'), NODE_KEY);
+		writeFileSync(join(__dirname, '../', 'keys/ca.key'), NODE_CA);
+		writeFileSync(join(__dirname, '../', 'keys/node.cert'), NODE_CERT);
+
+		console.log(`To install this node, login on the panel, and enter the IP and port (${port}) of this server!`);
+		console.log('The certificate can be found below! (copy the -----BEGIN CERTIFICATE----- and -----END CERTIFICATE----- too!)');
+		console.log('');
+		return console.log(NODE_CERT);
+	});
 }
 
 const app = express();
@@ -48,125 +59,52 @@ app
 	.use(express.urlencoded({ limit: '2000mb', extended: true }))
 	.set('views', join(__dirname, 'views'))
 	.set('view engine', 'ejs')
-	.get('/', async (req, res) => {
-		if (!SERVER_PUBLIC_KEY) {
-			res.status(200).render('index', {
-				publickey: NODE_PUBLIC_KEY,
-			});
-		} else {
-			return res.status(403).send('Please use the panel!');
-		}
-	})
 	.post('/init', (req, res) => {
-		if (!req.body || !req.body.encrypted || !req.body.key) return res.status(400).json({ message: NOT_ENCRYPTED_REQUEST, success: false });
+		const key = req.body.key;
+		if (!key) return res.status(400).json({ message: INVALID_BODY, success: false });
 
-		const encryptedbody = req.body;
-		const body = JSON.parse(unpack(encryptedbody));
+		if (PANEL_KEY && PANEL_KEY !== key) return res.status(403).json({ message: ALREADY_CONNECTED_TO_PANEL, success: false });
 
-		const newPublicServerKey = body.publickey;
-
-		if (SERVER_PUBLIC_KEY && SERVER_PUBLIC_KEY !== newPublicServerKey) {
-			const json = {
-				message: ALREADY_CONNECTED_TO_PANEL,
-				success: false,
-			};
-
-			const encryptedjson = pack(newPublicServerKey, json);
-			return res.status(403).json(encryptedjson);
+		if (!PANEL_KEY) {
+			PANEL_KEY = key;
+			writeFileSync(join(__dirname, '../', 'keys/panel'), key);
 		}
 
-		SERVER_PUBLIC_KEY = newPublicServerKey;
-		writeFileSync(join(__dirname, '../', 'keys/server_rsa_key.pub'), newPublicServerKey);
-
-		const json = {
-			message: SUCCESS,
-			success: true,
-		};
-
-		const encryptedjson = pack(newPublicServerKey, json);
-		return res.status(200).json(encryptedjson);
+		return res.status(200).json({ message: SUCCESS, success: true });
 	})
 	.post('/files/delete', async (req, res) => {
-		if (!req.body || !req.body.encrypted || !req.body.key) return res.status(400).json({ message: NOT_ENCRYPTED_REQUEST, success: false });
-		if (!SERVER_PUBLIC_KEY) return res.status(400).json({ message: NOT_CONNECTED_TO_PANEL, success: false, reconnect: true });
+		if (!PANEL_KEY) return res.status(400).json({ message: NOT_CONNECTED_TO_PANEL, success: false, reconnect: true });
 
-		const encryptedbody = req.body;
-		const body = JSON.parse(unpack(encryptedbody));
+		const id = req.body.id;
+		if (!id) return res.status(400).json({ message: INVALID_BODY, success: false });
+		if (!existsSync(`${dir}/${id}`)) return res.status(400).json({ message: NO_SUCH_FILE_OR_DIR, success: false });
 
-		if (!body.id) {
-			const json = {
-				message: INVALID_BODY,
-				success: false,
-			};
-			const encryptedjson = pack(SERVER_PUBLIC_KEY, json);
+		unlinkSync(`${dir}/${id}`);
 
-			return res.status(400).json(encryptedjson);
-		}
-
-		const dir = join(__dirname, '../', 'files');
-
-		if (!existsSync(`${dir}/${body.id}`)) {
-			const json = {
-				message: NO_SUCH_FILE_OR_DIR,
-				success: false,
-			};
-			const encryptedjson = pack(SERVER_PUBLIC_KEY, json);
-
-			return res.status(400).json(encryptedjson);
-		}
-
-		unlinkSync(`${dir}/${body.id}`);
-
-		const json = {
-			message: SUCCESS,
-			success: true,
-		};
-
-		const encryptedjson = pack(SERVER_PUBLIC_KEY, json);
-		return res.status(200).json(encryptedjson);
+		return res.status(200).json({ message: SUCCESS, success: true });
 	})
 	.post('/files/upload', upload.single('file'), async (req, res) => {
+		if (!PANEL_KEY) return res.status(400).json({ message: NOT_CONNECTED_TO_PANEL, success: false, reconnect: true });
 		const json = {
 			message: SUCCESS,
 			success: true,
 		};
-
-		const encryptedjson = pack(SERVER_PUBLIC_KEY, json);
-		return res.status(200).json(encryptedjson);
+		return res.status(200).json(json);
 	})
 	.post('/files/download', async (req, res) => {
-		if (!req.body || !req.body.encrypted || !req.body.key) return res.status(400).json({ message: NOT_ENCRYPTED_REQUEST, success: false });
-		if (!SERVER_PUBLIC_KEY) return res.status(400).json({ message: NOT_CONNECTED_TO_PANEL, success: false, reconnect: true });
+		if (!PANEL_KEY) return res.status(400).json({ message: NOT_CONNECTED_TO_PANEL, success: false, reconnect: true });
 
-		const encryptedbody = req.body;
-		const body = JSON.parse(unpack(encryptedbody));
+		const id = req.body.id;
+		if (!id) return res.status(400).json({ message: INVALID_BODY, success: false });
+		if (!existsSync(`${dir}/${id}`)) return res.status(400).json({ message: NO_SUCH_FILE_OR_DIR, success: false });
 
-		if (!body.id) {
-			const json = {
-				message: INVALID_BODY,
-				success: false,
-			};
-			const encryptedjson = pack(SERVER_PUBLIC_KEY, json);
-
-			return res.status(400).json(encryptedjson);
-		}
-
-		const dir = join(__dirname, '../', 'files');
-
-		if (!existsSync(`${dir}/${body.id}`)) {
-			const json = {
-				message: NO_SUCH_FILE_OR_DIR,
-				success: false,
-			};
-			const encryptedjson = pack(SERVER_PUBLIC_KEY, json);
-
-			return res.status(400).json(encryptedjson);
-		}
-
-		return createReadStream(`${dir}/${body.id}`).pipe(res);
-		// return res.status(200).send();
+		return createReadStream(`${dir}/${id}`).pipe(res);
 	})
-	.listen(port, (err) => {
-		if (err) console.log(err);
-		else console.log(`Server online on port ${port}`);
+	.get('*', (req, res) => {
+		return res.status(200).send('Please use the panel!');
 	});
+
+createServer({ key: NODE_KEY, cert: NODE_CERT, ca: NODE_CA }, app).listen(port, (err) => {
+	if (err) console.log(err);
+	else console.log(`Server online on port ${port}`);
+});
